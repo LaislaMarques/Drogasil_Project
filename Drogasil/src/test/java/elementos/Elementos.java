30c77f99473b4db0074f19eeb4e40d6d8e785116
Chimull Alves <rsppnet@hotmail.com>
2021-07-02 00:35:57 -0300

    OK

--- Drogasil/src/test/java/elementos/Elementos.java
+++ Drogasil/src/test/java/elementos/Elementos.java
@@ -6,11 +6,11 @@ public class Elementos {
 	
  private By cookies = By.id("onetrust-accept-btn-handler");
  private By onsite = By.xpath("//*[@id=\"sm__onsite\"]/div[3]/a");
- private By searchHeader = By.id("searchHeader");
- private By button = By.xpath("//*[@id=\"fixed-container-styles\"]/header/div/form/button[2]");
+ private By login = By.xpath("//*[@id=\"fixed-container-styles\"]/header/div/div[1]/div[1]/div/p");
+ private By button = By.xpath("//*[@id=\"fixed-container-styles\"]/header/div/div[1]/div[2]/button");
 
-public By getSearchHeader() {
-	return searchHeader;
+public By getlogin() {
+	return login;
 }
 
 public By getButton() {
